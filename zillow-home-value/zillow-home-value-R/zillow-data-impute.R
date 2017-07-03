90575e540f35faa21bba8c258b2b1d72cefdd51d
vinaybenny <vinaybenny@gmail.com>
2017-07-03 19:47:09 +1200

    code with no PCA-only imputations

--- zillow-home-value/zillow-home-value-R/zillow-data-impute.R
+++ zillow-home-value/zillow-home-value-R/zillow-data-impute.R
@@ -3,7 +3,8 @@ library(FactoMineR)
 
 # Drop columns with a lot of missing values
 missing_values <- properties %>% summarize_all(funs(sum(is.na(.))/n())) %>% gather(key="feature", value="missing_pct") %>% arrange(missing_pct)
-good_features <- filter(missing_values, missing_pct<=0.25)
+good_features <- filter(missing_values, missing_pct<=0.33)
+intermediate_features <- filter(missing_values, missing_pct > 0.33, missing_pct <=0.75)
 
 # Create a dataset for modelling purposes with extra variables as required
 # Drop the following columns
@@ -18,8 +19,11 @@ good_features <- filter(missing_values, missing_pct<=0.25)
 # tax_delinquency : is not required as information captured is redundant in tax_delinquency_year
 # censustractandblock : not surehow this is different from rawcensusandblock , and unable to understand the data. Need to revisit later
 
-excludecolumns <- c("id_parcel", "zoning_property", "zoning_landuse_county",
-                    "region_zip", "region_neighbor", "region_city", "tax_year", "tax_delinquency", "censustractandblock")
+excludecolumns <- c("id_parcel"
+                    ,"zoning_property", "zoning_landuse_county","region_zip", "region_neighbor", "region_city"
+                    ,"tax_year", "tax_delinquency"
+                    ,"censustractandblock"
+                    )
 
 propsubset <- properties %>% 
   select(good_features$feature) %>%
@@ -38,9 +42,15 @@ propsubsetimpute <- imputeFAMD(propsubset, ncp=5, verbose=TRUE)
 famdval <- FAMD(propsubset, tab.comp = propsubsetimpute$tab.disj, ncp = 20)
 
 
-propsubset <- data.table(famdval$svd$U)
+#propsubset <- data.table(famdval$svd$U)
+propsubset <- famdval$ind$coord
+propsubset <- propsubsetimpute$completeObs
 names(propsubset) <- rownames(famdval$eig)
 propsubset$id_parcel <- properties$id_parcel
+
+# Add in extra attributes for modelliing
+propsubset <-  propsubset %>% 
+  left_join( select(properties, id_parcel, excludecolumns[-1], intermediate_features$feature), by="id_parcel")
 
 
 # Join transactions with properties to create full dataset for analysis
